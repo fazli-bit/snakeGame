8adc970cd9dd32491a509ae63e540aae61cfb37e
fazli-bit <fazli@planmalaysia.gov.my>
2025-11-15 16:45:23 +0800

    Delete iceclimber directory

--- iceclimber/ice/game.js
+++
@@ -1,7 +0,0 @@
-// Fail ini mencipta dan mengeksport objek Game kosong.
-// Modul-modul lain akan mengimport ini dan melampirkan (attach)
-// logik mereka kepadanya (cth: Game.Core = { ... }).
-// Ini mengekalkan rujukan kod asal (cth: Game.Player.update())
-// tanpa memerlukan refactor logik yang besar.
-
-export const Game = {};
